99abb02e1f14dba92a2c80c14f1f7a547e53613e
Vidhyadharan Deivamani <vde@softwareag.com>
2022-07-26 21:56:50 +0530

    Added for make methods

---
+++ hello/making-slices.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := make([]int, 5)
+	printSlice("a", a)
+}
+
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v \n", s, len(x), cap(x), x)
+}
